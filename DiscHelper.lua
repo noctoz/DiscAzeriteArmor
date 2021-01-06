21ebda8c9adf7705195d6332fd08e0cea9538936
noctoz <noctoz@hotmail.com>
2021-01-06 14:41:59 +0100

    First iteration of Shadowlands support
    - Removed Azerite information
    - Added framework for showing Conduit information

--- DiscHelper.lua
+++ DiscHelper.lua
@@ -4,7 +4,7 @@ local addonName, n = ...
 local GetItemInfo = GetItemInfo
 local GetItemQualityColor = GetItemQualityColor
 
-function DiscAzeriteArmor_GetSpellID(powerID)
+function DiscHelper_GetSpellID(powerID)
 	local powerInfo = C_AzeriteEmpoweredItem.GetPowerInfo(powerID)
   	if (powerInfo) then
     	local azeriteSpellID = powerInfo["spellID"]
@@ -12,7 +12,7 @@ function DiscAzeriteArmor_GetSpellID(powerID)
   	end
 end
 
-function DiscAzeriteArmor_GetBagPosition(itemLink)
+function DiscHelper_GetBagPosition(itemLink)
     for bag = 0, NUM_BAG_SLOTS do
         for slot = 1, GetContainerNumSlots(bag) do
             if(GetContainerItemLink(bag, slot) == itemLink) then
@@ -23,179 +23,13 @@ function DiscAzeriteArmor_GetBagPosition(itemLink)
 end
 
 local OnTooltipSetItem = function(self, ...)
+	--print("Entered OnTooltipSetItem")
 	local name, itemLink = self:GetItem()
 	if itemLink then
+		--print("ItemLink valid")
 		local _, _, quality, itemLevel, _, _, _, _, itemEquipLoc = GetItemInfo(itemLink)
 		
-		if C_AzeriteEmpoweredItem.IsAzeriteEmpoweredItemByID(itemLink) then
-			local specID = GetSpecializationInfo(GetSpecialization())
-			local allTierInfo = C_AzeriteEmpoweredItem.GetAllTierInfoByItemID(itemLink)
-			if not allTierInfo then return end
-			
-			-- Create data source from item link and get the item
-			local owningItemSource = AzeriteEmpoweredItemDataSource:CreateFromFromItemLink(itemLink);
-			local sourceItem = owningItemSource:GetItem();
-			if not sourceItem:IsItemDataCached() then
-				-- We'll try again later
-				return false;
-			end
-			
-			local actualItemLocation = ItemLocation:CreateEmpty()
-			
-			-- Get the item location for the type of item (helm, shoulder, chest) and make sure it is an azerite item
-			local equipmentSlot = sourceItem:GetInventoryType()
-			-- For some reason there is a special "Robe" type that is not handled well so we need to remap that to "Chest"
-			if equipmentSlot == 20 then
-				equipmentSlot = 5
-			end
-			--print("InventoryType: "..equipmentSlot)
-			local equippedItemLocation = ItemLocation:CreateFromEquipmentSlot(equipmentSlot);
-			if not C_Item.DoesItemExist(equippedItemLocation) then
-				--print("Could not find item in slot")
-				return false
-			end
-			
-			if C_AzeriteEmpoweredItem.IsAzeriteEmpoweredItem(equippedItemLocation) then
-				--local equippedItemSource = AzeriteEmpoweredItemDataSource:CreateFromItemLocation(equippedItemLocation);
-				-- If the equipped item is the one we are looking at, select the equipped item location
-				if GetInventoryItemLink("player", equipmentSlot) == itemLink then
-					--print("Setting location from equipped!")
-					actualItemLocation = equippedItemLocation;
-				else
-					-- If the equipped location was not the item we are showing, find it in the bags
-					local bag, slot = DiscAzeriteArmor_GetBagPosition(itemLink)
-					if bag == nil or slot == nil then
-						--print("Error! Could not find item location!")
-					else
-						--print("Setting location from bag and slot!")
-						actualItemLocation = ItemLocation:CreateFromBagAndSlot(bag, slot)
-					end
-				end
-			else
-				-- If not azerite item equipped in slot, find it in the bags
-				local bag, slot = DiscAzeriteArmor_GetBagPosition(itemLink)
-				if bag == nil or slot == nil then
-					--print("Error! Could not find item location!")
-				else
-					--print("Setting location from bag and slot!")
-					actualItemLocation = ItemLocation:CreateFromBagAndSlot(bag, slot)
-				end
-			end
-			
-			-- Create some separation from other tooltip text
-			self:AddLine(" ")
-			local debugText = "Intellect from Azerite Powers:"
-			self:AddLine(debugText)
-			
-			local selectedInt = 0
-			local totalInt = 0
-			
-			-- Figure out how many tiers we have
-			local tierCount = 0
-			for k, v in pairs(allTierInfo) do
-				tierCount = tierCount + 1
-			end
-			--print("Tier count"..tierCount)
-			
-			local stackedInfo = {}
-			local twoDotInfo = {}
-			
-			for j=1, 4 do
-				local tierInfo = allTierInfo[j];
-				
-				-- If we have 5 tiers then the first two are the same
-				local powerDataIndex = j
-				if (tierCount == 5 and j > 1) then
-					powerDataIndex = j - 1
-				end
-				
-				local highestTierInt = 0
-				
-				local azeritePowerID = tierInfo["azeritePowerIDs"][1]
-
-				if azeritePowerID == 13 then break end -- Ignore +5 item level tier
-				
-				local azeriteTooltipText = " "
-				
-				for powerIndex, azeritePowerID in ipairs(tierInfo.azeritePowerIDs) do
-					local azeriteSpellID = DiscAzeriteArmor_GetSpellID(azeritePowerID)				
-					local azeritePowerName, _, icon = GetSpellInfo(azeriteSpellID)
-					-- Enable this to get new azerite ids
-					--print(azeritePowerName..": "..azeritePowerID)
-					
-					-- 404 is Death Throes that needs to be specially included since it is not considered a disc power
-					if C_AzeriteEmpoweredItem.IsPowerAvailableForSpec(azeritePowerID, specID) or azeritePowerID == 404 then
-						-- Uncomment to debug
-						--azeriteTooltipText = azeriteTooltipText.."|cFFFF0000["..powerDataIndex..", "..azeritePowerID..", "..itemLevel.."]|r "
-
-						--if n.powerData[powerDataIndex][azeritePowerID] then
-						--	azeriteTooltipText = azeriteTooltipText.."[a]"
-						--	if n.powerData[powerDataIndex][azeritePowerID]["intValues"][itemLevel] then
-						--		azeriteTooltipText = azeriteTooltipText.."[b]"
-						--	end
-						--end
-						-- End debug
-
-						local intValue = n.powerData[powerDataIndex][azeritePowerID] and n.powerData[powerDataIndex][azeritePowerID]["intValues"][itemLevel] or 0
-						-- We get the highest value to present the total int of the item
-						if intValue > highestTierInt then
-							highestTierInt = intValue
-						end
-						
-						-- Set color only for selected powers
-						local nameColor = "777777"
-						local intColor = "777777"
-						if actualItemLocation:HasAnyLocation() and C_AzeriteEmpoweredItem.IsPowerSelected(actualItemLocation, azeritePowerID) then
-							nameColor = "FFFFFF"
-							intColor = "00FF00"
-							selectedInt = selectedInt + intValue
-						end
-						
-						azeriteTooltipText = azeriteTooltipText.." |cFF"..nameColor..azeritePowerName.." |cFF"..intColor.."+"..intValue
-						
-						-- Add info for stacked
-						if n.powerData[powerDataIndex][azeritePowerID] and n.powerData[powerDataIndex][azeritePowerID]["stacked"] then
-							--print("Found stacked trait "..azeritePowerName)
-							azeriteTooltipText = azeriteTooltipText.."*"
-							table.insert(stackedInfo, azeritePowerID)
-						end
-						
-						-- Add info for two dots
-						if n.powerData[powerDataIndex][azeritePowerID] and n.powerData[powerDataIndex][azeritePowerID]["twoDot"] then
-							--print("Found stacked trait "..azeritePowerName)
-							azeriteTooltipText = azeriteTooltipText.."*"
-							table.insert(twoDotInfo, azeritePowerID)
-						end
-						
-						azeriteTooltipText = azeriteTooltipText.."|r"
-					end			
-				end
-				
-				totalInt = totalInt + highestTierInt
-				self:AddLine(azeriteTooltipText)
-			end
-			
-			local stats = GetItemStats(itemLink)
-			
-			self:AddLine("From Selected Powers: |cFF00FF00+"..selectedInt.." Intellect|r")
-			self:AddLine("Optimal From Powers: |cFF00FF00+"..totalInt.." Intellect|r")
-			self:AddLine("Total: |cFFFFFFFF+"..(stats["ITEM_MOD_INTELLECT_SHORT"] + selectedInt).." Intellect|r")
-			self:AddLine("Optimal: |cFFFFFFFF+"..(stats["ITEM_MOD_INTELLECT_SHORT"] + totalInt).." Intellect|r")
-			self:AddLine(" ")
-			
-			-- Write stacked info
-			for i, info in ipairs(stackedInfo) do
-				self:AddLine("*|cFFFFFFFF"..n.powerData[1][info]["name"].."|r will only provide |cFFFFFFFF+"..n.powerData[1][info]["stacked"][itemLevel].."|r if stacked.")
-			end
-			
-			-- Write two dot info
-			for i, info in ipairs(twoDotInfo) do
-				self:AddLine("*|cFFFFFFFF"..n.powerData[1][info]["name"].."|r will provide |cFFFFFFFF+"..n.powerData[1][info]["twoDot"][itemLevel].."|r with 2 DoTs.")
-			end
-			
-			-- Create some spacing after
-			self:AddLine(" ")
-		elseif itemEquipLoc == "INVTYPE_TRINKET" then
+		if itemEquipLoc == "INVTYPE_TRINKET" then
 			local itemString = string.match(itemLink, "item[%-?%d:]+")
 			local _, itemIdString, _, _, _, _, _, _, _, _, _, _, _, _ = strsplit(":", itemString)
 			itemId = tonumber(itemIdString)
@@ -214,6 +48,22 @@ local OnTooltipSetItem = function(self, ...)
 	end
 end
 
+local OnConduitTooltip = function(self, ...)
+	--print("Entered OnConduitTooltip")
+	conduitID = select(1, ...)
+	print("ConduitID: "..conduitID)
+	spellID = C_Soulbinds.GetConduitSpellID(conduitID, select(2, ...))
+	print("SpellID: "..spellID)
+	conduitRank = C_Soulbinds.GetConduitRankFromCollection(conduitID)
+	print("ConduitRank: "..conduitRank)
+	itemLevel = C_Soulbinds.GetConduitItemLevel(conduitID, conduitRank)
+	print("ItemLevel: "..itemLevel)
+	-- Create some separation from other tooltip text
+	self:AddLine(" ")
+	hpsGain = 99
+	self:AddLine("Estimated HPS Gain: |cFF00FF00+"..hpsGain.."|r")
+end
+
 for _, obj in next, {
 	GameTooltip,
 	ShoppingTooltip1,
@@ -227,5 +77,8 @@ for _, obj in next, {
 		obj:HookScript("OnTooltipSetItem", OnTooltipSetItem)
 	end
 end
+
+--hooksecurefunc("TaskPOI_OnEnter", OnConduitTooltip)
+hooksecurefunc(GameTooltip, "SetConduit", OnConduitTooltip)
 
 print("|cFF4863A0DiscAzeriteArmor 1.11.0 loaded. Data updated January 23th 2020.")
